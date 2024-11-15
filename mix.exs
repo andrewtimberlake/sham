48bc3f1b1803b15fba20bae1cb35992269c1c586
Andrew Timberlake <andrew@andrewtimberlake.com>
2024-11-15 10:37:49 +0200

    Fix telemetry errors

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule Sham.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :ssl],
+      extra_applications: [:logger, :ssl, :telemetry],
       mod: {Sham.Application, []}
     ]
   end
