8e1faf6367ea89681d7878942bc5d13ed5771467
Andrew Timberlake <andrew@andrewtimberlake.com>
2025-07-11 15:14:40 +0200

    Start Bandit application if it is available

--- lib/sham/application.ex
+++ lib/sham/application.ex
@@ -3,6 +3,10 @@ defmodule Sham.Application do
   use Application
 
   def start(_type, _args) do
+    if Code.ensure_loaded?(Bandit) do
+      Application.ensure_all_started(:bandit)
+    end
+
     DynamicSupervisor.start_link(strategy: :one_for_one, name: Sham.Supervisor)
   end
 end
